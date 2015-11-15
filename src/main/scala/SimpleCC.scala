7d7c6e78eda9d52ccedeadf177f3ab395279352e
Jakub Sawicki <kuba@jswk.pl>
2015-11-15 12:11:41 +0100

    Linearized graph

--- src/main/scala/SimpleCC.scala
+++ src/main/scala/SimpleCC.scala
@@ -10,7 +10,7 @@ object SimpleCC {
     val startMs = System.currentTimeMillis()
 
     //  Prepare data
-    val linksData = Array((1, 3), (2, 1), (3,1), (3,4), (4,1), (4,2), (5,6))
+    val linksData = Array((1, 2), (2, 3), (3, 4), (5,6))
 
     //  RDD of (url, url) pairs
     //  RDD[(String, String)]
