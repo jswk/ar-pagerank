18c99a7f57dbda1b86f9c7ca27b37925120584c5
Jakub Sawicki <kuba@jswk.pl>
2015-11-15 18:55:57 +0100

    Changed data paths

--- submit-cc-1.sh
+++ submit-cc-1.sh
@@ -2,10 +2,11 @@
 module load plgrid/apps/spark
 
 PROJ_PATH=$HOME/AR/lab2/ar-pagerank
+DATA_PATH=$HOME/AR/lab2/data
 
 $SPARK_HOME/bin/spark-submit \
     --class SparkCC \
     --master local[*] \
     ${PROJ_PATH}/target/scala-2.10/sparkpagerank_2.10-1.0.jar \
-    ${PROJ_PATH}/web-NotreDame.txt 1
+    ${DATA_PATH}/web-NotreDame.txt 1
 
--- submit-cc-2.sh
+++ submit-cc-2.sh
@@ -2,6 +2,7 @@
 module load plgrid/apps/spark
 
 PROJ_PATH=$HOME/AR/lab2/ar-pagerank
+DATA_PATH=$HOME/AR/lab2/data
 NODE_COUNT=$(uniq $PBS_NODEFILE | wc -l | awk '{print $1}')
 
 start-multinode-spark-cluster.sh
@@ -10,6 +11,6 @@ $SPARK_HOME/bin/spark-submit \
     --class SparkCC \
     --master spark://`hostname`:7077 \
     ${PROJ_PATH}/target/scala-2.10/sparkpagerank_2.10-1.0.jar \
-    ${PROJ_PATH}/web-NotreDame.txt $NODE_COUNT
+    ${DATA_PATH}/web-NotreDame.txt $NODE_COUNT
 
 stop-multinode-spark-cluster.sh
